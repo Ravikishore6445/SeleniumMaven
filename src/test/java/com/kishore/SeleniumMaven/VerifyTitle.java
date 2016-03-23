5f3ce76e614d0a1f54a6eb8c073cdcb2ea9a1a7d
kishoreanantatmula <kishore6445@gmail.com>
2016-03-23 18:07:53 +0530

    First commit

---
+++ src/test/java/com/kishore/SeleniumMaven/VerifyTitle.java
@@ -0,0 +1,25 @@
+package com.kishore.SeleniumMaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VerifyTitle {
+	
+	@Test
+	public void test123()
+	{
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://www.gmail.com");
+		driver.manage().window().maximize();
+		Assert.assertTrue(driver.getTitle().contains("Gmail"));
+		//driver.close();
+		driver.quit();
+		
+		
+	}
+
+}
